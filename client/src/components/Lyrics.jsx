25e59d536d8a0bde34084f4a6aa2d0eeb7f7ba3d
ashraf araj <ashrafthehero423@gmail.com>
2023-05-14 22:21:48 +0300

    added the styles to the header styles file

--- client/src/components/Lyrics.jsx
+++ client/src/components/Lyrics.jsx
@@ -1,45 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Header, LyricsSection, LyricsWrapper, Paragraph } from './HeaderStyle';
 
-const LyricsSection = styled.section`
-  display: flex;
-  margin-bottom: 2rem;
-`;
-
-const LyricsWrapper = styled.div`
-  width: 100%;
-  padding-right: 1rem;
-
-  &:last-child {
-    padding-right: 0;
-  }
-`;
-
-const Header = styled.h4`
-  margin-bottom: 0.5rem;
-  font-family: 'ABeeZee', sans-serif;
-`;
-
-const Paragraph = styled.p`
-  font-family: 'ABeeZee', sans-serif;
-`
 
 const Lyrics = () => {
   return (
     <LyricsSection>
       <LyricsWrapper>
         <Header>Translation</Header>
-        <p>
+        <Paragraph>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ab
           quod iure magni ut vitae dicta iste ad accusantium at.
-        </p>
+        </Paragraph>
       </LyricsWrapper>
       <LyricsWrapper>
         <Header>Original</Header>
-        <p>
+        <Paragraph>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ab
           quod iure magni ut vitae dicta iste ad accusantium at.
-        </p>
+        </Paragraph>
       </LyricsWrapper>
     </LyricsSection>
   );
