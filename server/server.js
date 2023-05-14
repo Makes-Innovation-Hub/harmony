99e4d351c6b7607df98431423c6d4a8da0348708
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-05-14 18:45:55 +0300

    #7 top songs page basic server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+dotenv.config({ path: path.join(__dirname, "config.env") });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
