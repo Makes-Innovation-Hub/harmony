e359da85fa9d021c6bf6bfa12df9bd443e46b2ca
Artur Labenskyy <Labenskyy@gmail.com>
2023-05-14 19:33:11 +0300

    translating animation fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,7 @@ import './App.css'
 
 import TranslatingAnimation from './components/TranslatingAnimation/TranslatingAnimation.component'
 function App() {
-    return (
-        <div>
-            <TranslatingAnimation />
-        </div>
-    )
+    return <div></div>
 }
 
 export default App
