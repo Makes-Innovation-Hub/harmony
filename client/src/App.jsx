3029cf5fd5915ac3bd93e1850dff2145a423e846
daniahih <daniahih310@gmail.com>
2023-05-14 16:20:39 +0300

    remove empty line

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home";
 import SongPage from "./Pages/SongPage";
 import ArtistPage from "./pages/ArtistPage";
-
-// import { useState } from "react";
-
+import { useState } from "react";
 const Router = createBrowserRouter([
   {
     path: "/",
@@ -22,21 +20,21 @@ const Router = createBrowserRouter([
 
 function App() {
    // the comment lie is just to see if the error boundary work 
-  // const [showError, setShowError] = useState(false);
+  const [showError, setShowError] = useState(false);
 
-  // const handleClick = () => {
-  //   setShowError(true);
-  // };
+  const handleClick = () => {
+    setShowError(true);
+  };
 
-  // if (showError) {
-  //   throw new Error("Oops! Something went wrong");
-  // }
+  if (showError) {
+    throw new Error("Oops! Something went wrong");
+  }
 
   return (
     <div>
       <RouterProvider router={Router} />
       <div>
-        {/* <button onClick={handleClick}>Click me</button> */}
+        <button onClick={handleClick}>Click me</button>
       </div>
     </div>
   );
