0396a8997cf92db6152e87a6d2d240b10bf4a00d
ashraf araj <ashrafthehero423@gmail.com>
2023-05-16 11:54:06 +0300

    #

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import Home from "./Pages/Home";
 import SongPage from "./Pages/SongPage";
 import ArtistPage from "./pages/ArtistPage";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 const Router = createBrowserRouter([
