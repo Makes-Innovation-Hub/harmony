f63a33d8a852b7f26d90366751a1dc766b5b4e11
ashraf araj <ashrafthehero423@gmail.com>
2023-05-16 10:58:32 +0300

    ignore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,6 @@ import { useState } from "react";
 import Header from "./Components/Header";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-     <Header/>
-    </>
 const Router = createBrowserRouter([
   {
     path: "/",
