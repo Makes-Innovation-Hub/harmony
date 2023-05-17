4faa684dbad5b50190d59fa48ff452d37953882d
ashraf araj <ashrafthehero423@gmail.com>
2023-05-17 15:41:55 +0300

    pulled new changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import Search from "../components/Searchbar";
 
 export default function Home() {
   return (
     <>
       <Header />
       <div>Home</div>
+      <Search/>
     </>
   );
 }
