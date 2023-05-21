646726366d2093d7c62c6ece6c400c1a81a47da3
Eyal-donut <eyallila2@gmail.com>
2023-05-21 11:16:45 +0300

    errorHandler

---
+++ server/middleware/errorHandler.js
@@ -0,0 +1,31 @@
+import ErrorResponse from '../utils/ErrorResponse.js';
+
+const errorHandler = (error, req, res, next) => {
+  let err = { ...error };
+
+  err.message = error.message;
+
+  console.log(error.stack.red);
+
+  if (error.name === 'CastError') {
+    const message = `Resource not found`;
+    err = new ErrorResponse(message, 404);
+  }
+
+  if (error.code === 11000) {
+    const message = 'Duplicate field value entered';
+    err = new ErrorResponse(message, 400);
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message).join(', ');
+    err = new ErrorResponse(message, 400);
+  }
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    error: err.message || 'Server Error'
+  });
+};
+
+export default errorHandler;
