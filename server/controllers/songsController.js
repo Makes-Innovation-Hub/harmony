5e0d0ae30bf6dd39ba8fef8d4469e534f752b39a
Eyal-donut <eyallila2@gmail.com>
2023-05-21 14:00:36 +0300

    songsController

--- server/controllers/songsController.js
+++ server/controllers/songsController.js
@@ -3,14 +3,17 @@ import Song from "../models/Song.js";
 import ErrorResponse from "../utils/ErrorResponse.js";
 import createSearchFilterObject from "../utils/createSearchFilterObject.js";
 
+const findSong = (async (req) => {
+    const filter = createSearchFilterObject(req)
+    const song = await Song.findOne(filter);
+    if (song) return song
+  });
+
 // @desc    Get a single song by name/artist/album
-//! @route   GET /api/v1...
+//@route    GET /api/v1/harmony/songs
 // @access  Public
 const getSong = asyncHandler(async (req, res, next) => {
-
-    const filter = createSearchFilterObject(req)
-
-    const song = await Song.findOne(filter);
+    const song = await findSong(req)
     if (!song) {
       return next(
         new ErrorResponse(
@@ -25,6 +28,30 @@ const getSong = asyncHandler(async (req, res, next) => {
     });
   });
 
-  
+// @desc    Create a Song
+// @route   POST /api/v1/harmony/songs
+// @access  dev
+export const createSong = asyncHandler(async (req, res, next) => {
+    const { name, originalLang, lyrics, artist } = req.body;
 
-  export {getSong}
+    //find artist by name to find artist id. If non-existent create new
+    //Add a function here that scrapes the song and returns the information bellow (name, lyrics...)
+  
+    const song = await Song.create({
+      name: {hebrew: name.hebrew, arabic:name.arabic, english:name.english.toLowerCase()},
+      lyrics: {hebrew: lyrics.hebrew, arabic:lyrics.arabic, english:lyrics.english},
+      originalLang,
+      artist
+    });
+    if (!song) {
+      return next(new ErrorResponse("Server error, song not created!"));
+    }
+    res.status(200).json({
+      success: true,
+      data: song,
+    });
+  });
+
+
+
+  export {getSong, createSong, findSong}
