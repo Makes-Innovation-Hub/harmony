60951f41cbdc1e350cc64d7c2dae7af1e88f84d2
daniahih <daniahih310@gmail.com>
2023-05-21 12:10:29 +0300

    fix issue

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
+import cors from 'cors'
 import express from "express";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
+import {connectDB, closeDBConnection} from "./config/db.js";
+import scrapeTopArabicSongs from "./scrapping/scrappingTopArabicSongs.js";
+import scrapeTopHebrewSongs from "./scrapping/scrappingTopHebrewSongs.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
+app.use(cors())
 import { getAlbumFromSongAndArtist } from './spotify.js'; 
 dotenv.config({ path: join(__dirname, "./config/config.env") });
 const PORT = process.env.PORT || 5000;
@@ -22,3 +27,20 @@ app.get('/album/:songName/:artistName', async (req, res) => {
     res.status(500).json({ error: 'An error occurred.' });
   }
 });
+app.get("/topArabicSongs", scrapeTopArabicSongs)
+app.get("/topHebrewSongs", scrapeTopHebrewSongs)
+const NODE_ENV = process.env.NODE_ENV
+
+let server;
+connectDB().then(() => {
+  server = app.listen(
+    PORT,
+    console.log(
+      `Server is running in ${NODE_ENV} mode on port ${PORT}`
+
+    )
+  );})
+  process.on("unhandledRejection", (err, promise) => {
+    console.error(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+    closeDBConnection()})
