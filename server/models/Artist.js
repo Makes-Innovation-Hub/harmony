18159226597a3922f68fcae786106d65df8c08f1
Eyal-donut <eyallila2@gmail.com>
2023-05-21 11:51:28 +0300

    Artist_mongoose_Schema

---
+++ server/models/Artist.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const ArtistSchema = new mongoose.Schema(
+    {
+        nameHebrew: {
+            type: String,
+            required: [true, "please add artist name in Hebrew"],
+            trim: true,
+            maxlength: [50, "Name can not be more than 50 characters"]
+        },
+        nameArabic: {
+            type: String,
+            required: [true, "please add artist name in Arabic"],
+            trim: true,
+            maxlength: [50, "Name can not be more than 50 characters"]
+        },
+        songs:[ 
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Song',
+            }
+        ],
+        imgURL: {
+            type: String,
+        },
+        albums: {
+            type: String,
+        }
+    },
+    {
+        toJSON: {
+          transform(_, ret) {
+            delete ret.__v;
+          },
+        },
+        toObject: {
+          transform(_, ret) {
+            delete ret.__v;
+          },
+        },
+      }
+
+)
+
+export default mongoose.model("Artist", ArtistSchema);
