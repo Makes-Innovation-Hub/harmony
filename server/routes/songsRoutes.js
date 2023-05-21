183762e16703fdf2649f35da49db5ef98041c231
Eyal-donut <eyallila2@gmail.com>
2023-05-21 15:06:55 +0300

    songsRouter

--- server/routes/songsRoutes.js
+++ server/routes/songsRoutes.js
@@ -1,8 +1,11 @@
 import express from 'express'
-import { getSong } from "../controllers/songsController.js";
+import { getSong, createSong } from "../controllers/songsController.js";
 
 const songsRouter = express.Router()
 
-songsRouter.route("/").get(getSong)
+songsRouter
+.route("/")
+.get(getSong)
+.post(createSong)
 
 export default songsRouter
