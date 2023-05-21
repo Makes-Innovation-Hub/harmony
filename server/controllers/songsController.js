84dc6487dcc0f188035c3117ecfa1aff70fd1616
Eyal-donut <eyallila2@gmail.com>
2023-05-21 14:16:14 +0300

    songsController

--- server/controllers/songsController.js
+++ server/controllers/songsController.js
@@ -31,15 +31,23 @@ const getSong = asyncHandler(async (req, res, next) => {
 // @desc    Create a Song
 // @route   POST /api/v1/harmony/songs
 // @access  dev
-export const createSong = asyncHandler(async (req, res, next) => {
+const createSong = asyncHandler(async (req, res, next) => {
     const { name, originalLang, lyrics, artist } = req.body;
 
     //find artist by name to find artist id. If non-existent create new
     //Add a function here that scrapes the song and returns the information bellow (name, lyrics...)
   
     const song = await Song.create({
-      name: {hebrew: name.hebrew, arabic:name.arabic, english:name.english.toLowerCase()},
-      lyrics: {hebrew: lyrics.hebrew, arabic:lyrics.arabic, english:lyrics.english},
+      name: {
+        hebrew: name.hebrew,
+         arabic:name.arabic,
+          english:name.english.toLowerCase()
+        },
+      lyrics: {
+        hebrew: lyrics.hebrew,
+         arabic:lyrics.arabic,
+          english:lyrics.english
+        },
       originalLang,
       artist
     });
@@ -52,6 +60,11 @@ export const createSong = asyncHandler(async (req, res, next) => {
     });
   });
 
+  const getOrCreateSong = async(req) => {
+    const song = findSong(req)
+    
+
+  }
 
 
   export {getSong, createSong, findSong}
