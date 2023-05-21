37850c8a263dfc4b24a015943c1c54a0e97a1fd0
daniahih <daniahih310@gmail.com>
2023-05-21 12:40:20 +0300

    test

--- server/server.js
+++ server/server.js
@@ -1,34 +1,24 @@
-import dotenv from "dotenv";
 import cors from 'cors'
+import dotenv from "dotenv";
 import express from "express";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 import {connectDB, closeDBConnection} from "./config/db.js";
 import scrapeTopArabicSongs from "./scrapping/scrappingTopArabicSongs.js";
 import scrapeTopHebrewSongs from "./scrapping/scrappingTopHebrewSongs.js";
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const app = express();
 app.use(cors())
-import { getAlbumFromSongAndArtist } from './spotify.js'; 
 dotenv.config({ path: join(__dirname, "./config/config.env") });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-app.get('/album/:songName/:artistName', async (req, res) => {
-  const { songName, artistName } = req.params;
-  try {
-    const albumName = await getAlbumFromSongAndArtist(songName, artistName);
-    const lyrics = await getLyricsForSong(songName, artistName);
-    res.json({ albumName, lyrics });
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'An error occurred.' });
-  }
-});
+
 app.get("/topArabicSongs", scrapeTopArabicSongs)
 app.get("/topHebrewSongs", scrapeTopHebrewSongs)
+
+const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV
 
 let server;
@@ -37,10 +27,15 @@ connectDB().then(() => {
     PORT,
     console.log(
       `Server is running in ${NODE_ENV} mode on port ${PORT}`
-
+        
     )
-  );})
-  process.on("unhandledRejection", (err, promise) => {
-    console.error(`Error: ${err.message}`);
-    server.close(() => process.exit(1));
-    closeDBConnection()})
+  );
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.error(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+  closeDBConnection()
+});
+
+
