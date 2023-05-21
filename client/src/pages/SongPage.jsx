f500e9b847cd323894e2436e08eb423b1f05a8b3
ashraf araj <ashrafthehero423@gmail.com>
2023-05-21 12:17:49 +0300

    moved lyrics from home to songs page

--- client/src/pages/SongPage.jsx
+++ client/src/pages/SongPage.jsx
@@ -1,11 +1,13 @@
 import SongGallary from "../components/SongGallary";
 import SongDetails from "../components/songDetails/SongDetails";
+import Lyrics from "../components/Lyrics"
 
 function SongPage() {
   return (
     <>
       <SongDetails />
       <SongGallary />
+      <Lyrics/>
     </>
   );
 }
