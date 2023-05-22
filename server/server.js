e16c03e1436fb1ea2c5dc16f487b23c92445dbec
daniahih <daniahih310@gmail.com>
2023-05-22 13:44:30 +0300

    open ai configuration

--- server/server.js
+++ server/server.js
@@ -4,29 +4,39 @@ import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import  { Configuration, OpenAIApi } from "openai";
+import {Configuration, OpenAIApi} from "openai";
 const app = express();
-dotenv.config({ path: join(__dirname, "config.env") });
+dotenv.config({ path: join(__dirname, "./config/config.env") });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const config = new Configuration({
+  apiKey: process.env.OPEN_AI_API_KEY
 });
-const openai = new OpenAIApi(configuration);
-
- async function start(){
+const openai = new OpenAIApi(config);
+const runPrompt = async (question) => {
+  const prompt = question;
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "give me a essay about natural things ",
-    temperature: 0,
-    max_tokens: 1000,
+    prompt: prompt,
+    max_tokens: 2048,
+    temperature: 1
   });
-  console.log(response.data.choices[0].text);
+  const parsableJSONresponse = response.data.choices[0].text;
+  if (
+    question.includes("from english to hebrew") ||
+    question.includes("from english to arabic")
+  ) {
+    return parsableJSONresponse.split("").reverse().join("");
+  } else {
+    return parsableJSONresponse;
+  }
+};
 
- }
-start()
+const question_EN_HE = `translate from english to hebrew : "Hello World 1"`;
+
+const result = await runPrompt(question_EN_HE);
+console.log(result);
