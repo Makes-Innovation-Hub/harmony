05c22f81daced8c87ea7ac8ba80fb893dced5716
Eyal-donut <eyallila2@gmail.com>
2023-05-22 15:33:52 +0300

    topSongs

--- server/models/TopSongs.js
+++ server/models/TopSongs.js
@@ -6,14 +6,13 @@ const TopSongsSchema = new mongoose.Schema(
             type: Date,
             default: Date.now,
         },
+        language: {
+          type: String,
+          required: [true, "Please add language"],
+          enum: ['hebrew', 'arabic']
+        },
         //Before creating the playlist first look for all the songs in Mongo. If a song doesn't exist, create it. When creating a playlist, use populate to refer to the songs by id
-        topHebrew:[ 
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Song',
-            }
-        ],
-        topArabic:[ 
+        topSongs:[ 
             {
                 type: mongoose.Schema.Types.ObjectId,
                 ref: 'Song',
