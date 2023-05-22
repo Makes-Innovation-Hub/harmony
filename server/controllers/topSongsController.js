fd1f83e2618f69bce283a1a6a1dc3de2427ed809
Eyal-donut <eyallila2@gmail.com>
2023-05-22 15:13:07 +0300

    controllers_updated

--- server/controllers/topSongsController.js
+++ server/controllers/topSongsController.js
@@ -5,4 +5,16 @@ import createSongOrArtistObject from "../utils/controllersUtils.js";
 import scrapeTopArabicSongs from "../scrapping/scrappingTopArabicSongs.js";
 import scrapeTopHebrewSongs from "../scrapping/scrappingTopHebrewSongs.js";
 
-import { findSong } from "./songsController.js";
+import { findSong, getOrCreateSongAndReturn } from "./songsController.js";
+
+// @desc    Get songs by name/artist/album
+//@route    GET /api/v1/harmony/songs
+// @access  Public
+const getTopArabic = asyncHandler(async (req, res, next) => {
+    const scrapedTopSongs = JSON.parse(scrapeTopArabicSongs())
+
+    //The results should be massaged here, to get an array of top songs, and each song should be an object with the exact structure and information in the model (Song.js)
+    const dummyResults = []
+
+    const songsArray = await getOrCreateSongAndReturn(req)
+  });
