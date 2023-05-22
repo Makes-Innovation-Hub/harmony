18b224dfa31e07d8b4aeec382920ea2433ac7659
daniahih <daniahih310@gmail.com>
2023-05-22 11:29:08 +0300

    test configuration

--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
+import  { Configuration, OpenAIApi } from "openai";
 const app = express();
-
 dotenv.config({ path: join(__dirname, "config.env") });
 
 const PORT = process.env.PORT || 5000;
@@ -14,3 +13,20 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+ async function start(){
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "give me a essay about natural things ",
+    temperature: 0,
+    max_tokens: 1000,
+  });
+  console.log(response.data.choices[0].text);
+
+ }
+start()
