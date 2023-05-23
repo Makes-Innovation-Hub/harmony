e897f17919618002b7f524a028d60c088def17e1
daniahih <daniahih310@gmail.com>
2023-05-23 20:59:03 +0300

    open ai configuration new request

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 import {connectDB, closeDBConnection} from "./config/db.js";
 import scrappingRoutes from './routes/scrappingRoutes.js'
-import pkg from "openai";
-const { Configuration, OpenAIApi } = pkg;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -30,59 +28,8 @@ app.listen(
   mode on port ${PORT}`)
 );
 
-
 process.on("unhandledRejection", (err, promise) => {
   console.error(`Error: ${err.message}`);
   server.close(() => process.exit(1));
   closeDBConnection()
 });
-
-const init = () => {
-  const config = new Configuration({
-    apiKey: process.env.OPEN_AI_API_KEY,
-  });
-  return new OpenAIApi(config);
-};
-
-const openai = init();
-const openAiInit = async (openai, prompt) => {
-  try {
-    return await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
-    });
-  } catch (error) {
-    throw new Error("OpenAI API request failed: " + error.message);
-  }
-};
-const runPrompt = async (question) => {
-  const prompt = question;
-  try {
-    const response = await openAiInit(openai, prompt);
-    const parsableJSONresponse = response.data.choices[0].message.content;
-
-    if (
-      question.includes("from english to hebrew") ||
-      question.includes("from english to arabic")
-    ) {
-      return parsableJSONresponse.split("").reverse().join("");
-    } else {
-      return parsableJSONresponse;
-    }
-  } catch (error) {
-    throw new Error("Failed to process prompt: " + error.message);
-  }
-};
-
-// Example usage
-const question = `translate from hebrew to arabic: "שלום עולם"`;
-
-
-runPrompt(question)
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error("An error occurred:", error);
-  });
-
