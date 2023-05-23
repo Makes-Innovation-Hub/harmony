61b66d0f09162d1ae2b4f2189db6249548dd015d
yishain11 <yishain11@walla.co.il>
2023-05-23 20:53:37 +0300

    mmit
    server cleanup

--- server/server.js
+++ server/server.js
@@ -1,45 +1,37 @@
-import cors from 'cors'
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
-import {connectDB, closeDBConnection} from "./config/db.js";
-import scrappingRoutes from './routes/scrappingRoutes.js'
-import songsRouter from './routes/songsRoutes.js';
-import artistsRouter from './routes/artistsRoutes.js';
-import topSongsRouter from './routes/topSongsRoutes.js';
-import errorHandler from './middleware/errorHandler.js';
-
+import { connectDB, closeDBConnection } from "./config/db.js";
+import scrappingRoutes from "./routes/scrappingRoutes.js";
+import songsRouter from "./routes/songsRoutes.js";
+import artistsRouter from "./routes/artistsRoutes.js";
+import topSongsRouter from "./routes/topSongsRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 dotenv.config({ path: join(__dirname, "./config/config.env") });
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome",
-  });
-});
-
-app.use("/api/v1/", scrappingRoutes)
-
-app.use("/api/v1/harmony/songs", songsRouter)
-app.use("/api/v1/harmony/artists", artistsRouter)
-app.use("/api/v1/harmony/topSongs", topSongsRouter)
+app.use("/api/v1/", scrappingRoutes);
+app.use("/api/v1/harmony/songs", songsRouter);
+app.use("/api/v1/harmony/artists", artistsRouter);
+app.use("/api/v1/harmony/topSongs", topSongsRouter);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-const NODE_ENV = process.env.NODE_ENV
+const NODE_ENV = process.env.NODE_ENV;
 
 let server;
 
-connectDB()
+connectDB();
 
 app.listen(
   PORT,
@@ -47,11 +39,8 @@ app.listen(
   mode on port ${PORT}`)
 );
 
-
 process.on("unhandledRejection", (err, promise) => {
   console.error(`Error: ${err.message}`);
+  closeDBConnection();
   server.close(() => process.exit(1));
-  closeDBConnection()
 });
-
-
