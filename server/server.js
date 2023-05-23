2c20d5db09565533f9f1ab5bb12fe4153630da61
daniahih <daniahih310@gmail.com>
2023-05-23 21:30:34 +0300

    added a route song

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 import {connectDB, closeDBConnection} from "./config/db.js";
 import scrappingRoutes from './routes/scrappingRoutes.js'
-import {getAlbumFromSongAndArtist, getCoverArtForSong} from './spotifyapi.js'; 
+import SongRoute from './routes/songRoutes.js'
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -19,7 +19,7 @@ dotenv.config({ path: join(__dirname, "./config/config.env") });
 
 
 app.use("/api/v1/", scrappingRoutes)
-
+app.use('/', SongRoute);
 
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV
@@ -33,28 +33,7 @@ app.listen(
   console.log(`Server running in ${NODE_ENV}
   mode on port ${PORT}`)
 );
-app.get('/:songName/:artistName', async (req, res) => {
-  const { songName, artistName } = req.params;
 
-  try {
-    let ArtistName = '';
-    const containsHebrew = /[א-ת]/.test(artistName);
-
-    if (!containsHebrew) {
-      ArtistName = artistName;
-    }
-
-    const [albumName, coverArt] = await Promise.all([
-      getAlbumFromSongAndArtist(songName, ArtistName),
-      getCoverArtForSong(songName, ArtistName)
-    ]);
-
-    res.json({ albumName, coverArt });
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'An error occurred.' });
-  }
-});
 
 
 process.on("unhandledRejection", (err, promise) => {
