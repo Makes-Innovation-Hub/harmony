bb9059f0e99b8ca81b06db9a4ed6a1512c43415b
daniahih <daniahih310@gmail.com>
2023-05-29 15:28:08 +0300

    song for the artist

--- server/routes/searchRoutes.js
+++ server/routes/searchRoutes.js
@@ -3,26 +3,27 @@ import Song from '../models/Song.js'
 import Artist from '../models/Artist.js';
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  const searchTerm = req.body.searchTerm;
-
-  Promise.all([
-    Song.find({
-      $or: [
-        { 'name.hebrew': { $regex: searchTerm, $options: 'i' } },
-        { 'name.arabic': { $regex: searchTerm, $options: 'i' } },
-        { 'name.english': { $regex: searchTerm, $options: 'i' } },
-      ],
-    }),
-    Artist.find({
-      $or: [
-        { 'name.hebrew': { $regex: searchTerm, $options: 'i' } },
-        { 'name.arabic': { $regex: searchTerm, $options: 'i' } },
-        { 'name.english': { $regex: searchTerm, $options: 'i' } },
-      ],
-    }),
-  ])
-    .then(([songResults, artistResults]) => {
+router.post('/', async (req, res) => {
+    const searchTerm = req.body.searchTerm;
+  
+    try {
+      const [songResults, artistResults] = await Promise.all([
+        Song.find({
+          $or: [
+            { 'name.hebrew': { $regex: searchTerm, $options: 'i' } },
+            { 'name.arabic': { $regex: searchTerm, $options: 'i' } },
+            { 'name.english': { $regex: searchTerm, $options: 'i' } },
+          ],
+        }),
+        Artist.find({
+          $or: [
+            { 'name.hebrew': { $regex: searchTerm, $options: 'i' } },
+            { 'name.arabic': { $regex: searchTerm, $options: 'i' } },
+            { 'name.english': { $regex: searchTerm, $options: 'i' } },
+          ],
+        }).populate('songs'),
+      ]);
+  
       const formattedResults = {
         songs: songResults.map((song) => ({
           id: song._id,
@@ -31,16 +32,19 @@ router.post('/', (req, res) => {
         artists: artistResults.map((artist) => ({
           id: artist._id,
           name: artist.name,
-  
+          songs: artist.songs.map((song) => ({
+            id: song._id,
+            name: song.name,
+          })),
         })),
       };
-
+  
       res.json(formattedResults);
-    })
-    .catch((error) => {
+    } catch (error) {
       console.error('Search error:', error);
       res.status(500).json({ error: 'Internal Server Error' });
-    });
-});
+    }
+  });
+  
 
 export default router;
