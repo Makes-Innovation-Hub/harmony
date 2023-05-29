da495717255d83cfc501c6b271158642f0b55b66
Eyal-donut <eyallila2@gmail.com>
2023-05-29 14:35:11 +0300

    artistPage_artistImage

---
+++ client/src/components/ContentWrapper/ContentWrapper.styler.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
+export default Wrapper
