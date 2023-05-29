2323cf54e0d50b8c63700d4deada48c4af943914
Eyal-donut <eyallila2@gmail.com>
2023-05-29 15:24:47 +0300

    artist_page

--- client/src/pages/ArtistPage.jsx
+++ client/src/pages/ArtistPage.jsx
@@ -15,10 +15,12 @@ function ArtistPage() {
     </FlexGrowContainer>
 
     <FlexGrowContainer flexGrow='90'>
-      <ClipArtImage width='100' height='100%' border='1px solid black' imgURL='https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg/1599px-%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg' />
+      <ClipArtImage width='100%' height='100%' border='1px solid' imgURL='https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg/1599px-%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg' />
     </FlexGrowContainer>
 
-    <FlexGrowContainer flexGrow='60'></FlexGrowContainer>
+    <FlexGrowContainer flexGrow='60'>
+      
+    </FlexGrowContainer>
   </ContentWrapper>
   )
 }
