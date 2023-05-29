f2ed52e372d1a7855dcd4c97e3617d60fe16cd03
Eyal-donut <eyallila2@gmail.com>
2023-05-29 12:43:02 +0300

    artistPage

--- client/src/components/ClipArtImage/ClipArtImage.styled.js
+++ client/src/components/ClipArtImage/ClipArtImage.styled.js
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
-export const Image = styled.div`
+ const Image = styled.div`
   border-radius: 35px;
-  width: 10rem;
-  height: 10rem;
-  background: url('') no-repeat center center cover;
+  
 `;
-
+export default Image
--- client/src/pages/ArtistPage.jsx
+++ client/src/pages/ArtistPage.jsx
@@ -7,7 +7,7 @@ function ArtistPage() {
   <>
   <Header/>
   <ThreeLangNames arabicName='تونة' hebrewName='טונה' englishName='Tuna'/>
-  <ClipArtImage width='5rem' height='5rem' imgURL='https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg/1599px-%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg'/>
+  <ClipArtImage width='5rem' height='5rem' imgURL='https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg/1599px-%D7%90%D7%99%D7%AA%D7%99_%D7%96%D7%91%D7%95%D7%9C%D7%95%D7%9F_%D7%98%D7%95%D7%A0%D7%94.jpg' />
 
   </>
   )
