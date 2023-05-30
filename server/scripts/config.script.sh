1a00cb06f792dc6055d36070226f21aba5ce4a5e
Artur Labenskyy <Labenskyy@gmail.com>
2023-05-30 16:07:25 +0300

    review changes

--- server/scripts/config.script.sh
+++ server/scripts/config.script.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 CONFIG_DIR="config"
 
 # Check if the config.env file already exists
-if [ -f "$config/config.env" ]; then
+if [ -f "$CONFIG_DIR/config.env" ]; then
   echo "config.env file already exists."
   echo "Please edit the existing config.env file to make changes."
 else
@@ -11,23 +9,16 @@ else
   mkdir -p "$CONFIG_DIR"
   touch "$CONFIG_DIR/config.env"
 
-  # Prompt the user to enter the required environment variables
-  echo "Enter the value for PORT:"
-  read -r PORT
+  # Define an array of environment variable names
+  declare -a variables=("PORT" "NODE_ENV" "MONGO_URI" "OPEN_AI_API_KEY")
 
-  echo "Enter the value for NODE_ENV:"
-  read -r NODE_ENV
-
-  echo "Enter the value for MONGO_URI:"
-  read -r MONGO_URI
-
-  echo "Enter the value for OPEN_AI_API_KEY:"
-  read -r OPEN_AI_API_KEY
-  # Write the environment variables to the config.env file
-  echo "PORT=$PORT" >> "$CONFIG_DIR/config.env"
-  echo "NODE_ENV=$NODE_ENV" >> "$CONFIG_DIR/config.env"
-  echo "MONGO_URI=$MONGO_URI" >> "$CONFIG_DIR/config.env"
-  echo "OPEN_AI_API_KEY=$OPEN_AI_API_KEY" >> "$CONFIG_DIR/config.env"
+  # Loop over the array and prompt the user to enter values for each variable
+  for variable in "${variables[@]}"
+  do
+    echo "Enter the value for $variable:"
+    read -r value
+    echo "$variable=$value" >> "$CONFIG_DIR/config.env"
+  done
 
   echo "config.env file created and configured successfully in the $CONFIG_DIR folder."
 fi
