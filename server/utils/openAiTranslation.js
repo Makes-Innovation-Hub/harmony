df6b08f27e1fb6767e551543dc872ebfce0adaff
Artur Labenskyy <Labenskyy@gmail.com>
2023-05-31 10:31:31 +0300

    update after review

--- server/utils/openAiTranslation.js
+++ server/utils/openAiTranslation.js
@@ -1,34 +1,38 @@
-import axios from 'axios'
+import axios from "axios";
 
+import { openAiUrl } from "../constants/urls.js";
 
-export async function translateLyricsByOpenAi(lyrics, originalLanguage, targetLanguage) {
-  const apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-  
+export async function translateLyricsByOpenAi(
+  lyrics,
+  originalLanguage,
+  targetLanguage
+) {
   const prompt = `Translate the following song lyrics from ${originalLanguage} to ${targetLanguage}:\n\n ${lyrics} \n\nTranslate:`;
   const messages = [
-    { role: 'system', content: prompt },
-    { role: 'user', content: lyrics },
+    { role: "system", content: prompt },
+    { role: "user", content: lyrics },
   ];
 
   try {
-    const response = await axios.post(apiUrl, {
-      messages: messages,
-      model: 'gpt-3.5-turbo',
-      max_tokens: 100,
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPEN_AI_API_KEY}`,
-        'Content-Type': 'application/json',
+    const response = await axios.post(
+      openAiUrl,
+      {
+        messages: messages,
+        model: "gpt-3.5-turbo",
+        max_tokens: 100,
       },
-    });
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPEN_AI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     const translatedText = response.data.choices[0].message.content;
     return translatedText;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     return null;
   }
 }
-
-
--- server/utils/translatingFunction.js
+++ server/utils/translatingFunction.js
@@ -5,82 +5,87 @@ import Artist from "../models/Artist.js";
 
 import { translateLyricsByOpenAi } from "./openAiTranslation.js";
 
-async function searchArtistSong(songName, artist) {
-    // try {
-    //   const query = {
-    //     originalLang: originalLang,
-    //     $or: [
-    //       { 'lyrics.hebrew': lyrics },
-    //       { 'lyrics.arabic': lyrics },
-    //       { 'lyrics.english': lyrics }
-    //     ]
-    //   };
-  
-    //   const song = await Song.findOne(query);
-    //   return song;
-    // } catch (error) {
-    //   console.error(error);
-    //   return null;
-    // }
-    try {
-      const songArtist = await Artist.findById(artist)
-      const song = songArtist.find({'songs.name': songName}).populate('songs')
-      return song;
-    } catch (error) {
-      console.error(error);
-      return null;
+let artistId;
+
+async function searchArtistSong(songName, artistName) {
+  try {
+    const artist = await Artist.findOne({
+      $or: [
+        { "name.hebrew": artistName },
+        { "name.arabic": artistName },
+        { "name.english": artistName },
+      ],
+    })
+      .populate({
+        path: "songs",
+      })
+      .exec();
+
+    if (!artist) {
+      throw new Error("Artist not found");
     }
+    artistId = artist._id;
+
+    const songArray = artist.songs.filter(
+      (song) =>
+        song.name.hebrew === songName ||
+        song.name.english === songName ||
+        song.name.arabic === songName
+    );
+    return songArray[0];
+  } catch (error) {
+    throw new Error(`Error finding artist's song: ${error.message}`);
   }
+}
 
 export const translatingFunction = asyncHandler(async (req, res, next) => {
-    const originalLanguage = req.body.originalLanguage
-    const artist = req.body.artist
-    const lyrics = req.body.lyrics
-    const targetLanguage = req.body.targetLanguage
-    const songName = req.body.songName
+  const originalLanguage = req.body.originalLanguage;
+  const artistName = req.body.artistName;
+  const lyrics = req.body.lyrics;
+  const targetLanguage = req.body.targetLanguage;
+  const songName = req.body.songName;
 
-    const song = await searchArtistSong(originalLanguage, songName, artist)
-    console.log("song is:", song)
+  const song = await searchArtistSong(songName, artistName);
 
-    if (song && song.lyrics[targetLanguage]) {
-        res.status(200).json({
-            success: true,
-            data: song.lyrics[targetLanguage]
-        })
+  if (song && song.lyrics[targetLanguage]) {
+    res.status(200).json({
+      success: true,
+      data: song.lyrics[targetLanguage],
+    });
+  } else {
+    const targetLyrics = await translateLyricsByOpenAi(
+      lyrics,
+      originalLanguage,
+      targetLanguage
+    );
+    if (targetLyrics) {
+      // Store the translated lyrics in the database
+      if (song) {
+        // Update the existing song record
+        song.lyrics[targetLanguage] = targetLyrics;
+        await song.save();
+      } else {
+        // Create a new song record
+        const newSong = new Song({
+          artist: artistId,
+          originalLang: originalLanguage,
+          lyrics: {
+            [targetLanguage]: targetLyrics,
+            [originalLanguage]: lyrics,
+          },
+        });
+        await newSong.save();
+      }
+
+      res.status(200).json({
+        success: true,
+        data: targetLyrics,
+      });
     } else {
-        const targetLyrics = await translateLyricsByOpenAi(lyrics, originalLanguage, targetLanguage)
-        if (targetLyrics) {
-            // Store the translated lyrics in the database
-            if (song) {
-              // Update the existing song record
-              song.lyrics[targetLanguage] = targetLyrics;
-              console.log('ok....')
-              await song.save();
-            } else {
-              // Create a new song record
-              const newSong = new Song({
-                artist: artist,
-                originalLang: originalLanguage,
-                lyrics: {
-                  [targetLanguage]: targetLyrics,
-                  [originalLanguage]: lyrics
-                },
-              });
-              console.log('cant save song...')
-              await newSong.save();
-            }
-      
-            res.status(200).json({
-              success: true,
-              data: targetLyrics,
-            });
-          } else {
-            res.status(500).json({
-              success: false,
-              error: "Failed to translate lyrics.",
-            });
-          }
-        }
+      res.status(500).json({
+        success: false,
+        error: "Failed to translate lyrics.",
+      });
+    }
+  }
 });
-
-
