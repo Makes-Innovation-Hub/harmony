4f0b364b819867ffeed9a1cd50474d835932ceb3
daniahih <daniahih310@gmail.com>
2023-05-31 13:06:55 +0300

    issue85Lyrics

--- server/genuisLyrics.js
+++ server/genuisLyrics.js
@@ -1,49 +1,57 @@
-import dotenv from 'dotenv';
+import express from 'express';
 import axios from 'axios';
-dotenv.config({ path: './config/config.env' });
+import cheerio from 'cheerio';
+import dotenv from 'dotenv';
+dotenv.config({ path:'./config/config.env' });
 
-async function getLyricsForSong(songName, artistName) {
+const app = express();
+
+async function fetchLyrics(searchTerm) {
+  const options = {
+    method: 'GET',
+    url: 'https://genius-song-lyrics1.p.rapidapi.com/search/',
+    params: {
+      q: searchTerm,
+      per_page: '20',
+      page: '1',
+    },
+    headers: {
+      'X-RapidAPI-Key':  process.env.RAPIDAPI_KEY,
+      'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
+    }
+  };
+
   try {
-    const response = await axios.get('https://api.genius.com/search', {
-      params: {
-        q: `${songName} ${artistName}`,
-      },
-      headers: {
-        Authorization: `Bearer ${process.env.GENIUS_API_KEY}`,
-      },
-    });
-    const searchResult = response.data.response.hits[0];
-    console.log(searchResult)
-    if (searchResult) {
-      const songId = searchResult.result.id;
-      const songResponse = await axios.get(`https://api.genius.com/songs/${songId}`, {
-        headers: {
-          Authorization: `Bearer ${process.env.GENIUS_API_KEY}`,
-        },
-      });
-      const lyricsData = songResponse.data.response.song;
-      if (lyricsData) {
-        return lyricsData.lyrics;
+    const response = await axios.request(options);
+    const hits = response.data.hits;
+
+    if (hits && hits.length > 0) {
+      const firstHit = hits[0];
+      const songResult = firstHit.result;
+      const lyricsUrl = `https://genius.com${songResult.path}`;
+
+      const lyricsResponse = await axios.get(lyricsUrl);
+      const $ = cheerio.load(lyricsResponse.data);
+
+      const lyrics = $('div[data-lyrics-container="true"]')
+        .html()
+        .replace(/<br>/g, '\n')
+        .trim();
+
+      if (lyrics.length > 0) {
+        const lines = lyrics.split('\n');
+        return lines;
       } else {
-        throw new Error('Lyrics not found.');
+        console.error('No lyrics found.');
+        return false;
       }
     } else {
-      throw new Error('Song not found.');
+      console.error('No search results found.');
+      return false;
     }
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error(error);
+    return false;
   }
 }
-
-async function exampleUsage() {
-  try {
-    const lyrics = await getLyricsForSong('Unicorn', 'Noa Kirel');
-    console.log('Lyrics:', lyrics);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-}
-
-exampleUsage();
-
-export { getLyricsForSong };
+export { fetchLyrics }
---
+++ server/tests/fetchLyrics.tests.js
@@ -0,0 +1,21 @@
+import chai from "chai";
+const expect = chai.expect;
+import { fetchLyrics } from '../genuisLyrics.js';
+
+describe('fetchLyrics', () => {
+  it('should return an array of lines when lyrics are found', async () => {
+    const searchTerm = 'אהבה חולה פאר טסי';
+    const lyrics = await fetchLyrics(searchTerm);
+
+    expect(Array.isArray(lyrics)).to.be.true;
+    expect(lyrics.length).to.be.greaterThan(0);
+    expect(typeof lyrics[0]).to.be.equal('string');
+  });
+``
+  it('should return false when no lyrics are found', async () => {
+    const searchTerm = 'צעדים עקיבא ';
+    const lyrics = await fetchLyrics(searchTerm);
+
+    expect(lyrics).to.be.false;
+  });
+});
