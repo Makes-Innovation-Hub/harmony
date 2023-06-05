8ead9b4df4ef2a2a3bdf6e87a7342329b8af32f1
Toni Zoaretz <tonizoaretz1@gmail.com>
2023-06-05 14:37:04 +0300

    google scrapping middle function

--- server/scrapping/scrappingGoogleLyrics.js
+++ server/scrapping/scrappingGoogleLyrics.js
@@ -17,32 +17,55 @@ const scrapeGoogleLyrics = async (songName, singerName) => {
 
   const links = await page.evaluate(() => {
     const linkElements = Array.from(document.querySelectorAll("div.yuRUbf a"));
-    return linkElements.map((element) => {
-      const href = element.href;
-      if (
-        href.startsWith("https://shironet.mako") ||
-        href.startsWith("https://genius.com/") ||
-        href.startsWith("https://www.tab4u.com/")
-      ) {
-        return href;
-      }
-    });
+
+    const filteredLinks = [];
+
+  linkElements.forEach((element) => {
+    const href = element.href;
+
+    if (href.startsWith("https://shironet.mako")) {
+      filteredLinks.push(href);
+    } else if (href.startsWith("https://genius.com/")) {
+      filteredLinks.push(href);
+    } else if (href.startsWith("https://www.tab4u.com/")) {
+      filteredLinks.push(href);                 
+    } else if (href.startsWith("https://www.lyrics-arabic.com/")) {
+      filteredLinks.push(href);            
+    } else if (href.startsWith("https://lyricstranslate.com/")) {
+      filteredLinks.push(href);
+    }                  
   });
 
-  const filteredLinks = links.filter(Boolean);
+  return filteredLinks;
+    // return linkElements.map((element) => {
+    //   const href = element.href;
+    //   if (
+    //     href.startsWith("https://shironet.mako")
+    //      ||
+    //     href.startsWith("https://genius.com/")
+    //   ) {
+    //     return href;
+    //   }
+    // });
+
+  });
+
+  const filteredLinks = links.filter(Boolean).slice(0,1)
+  console.log("THE FILTERDE LINK ARE", filteredLinks)
 
   const lyrics = [];
 
   for (const link of filteredLinks) {
+    
     await page.goto(link);
     await page.waitForSelector(
-      "span.artist_lyrics_text, .Lyrics__Container-sc-1ynbvzw-5, .song_only"
+      "span.artist_lyrics_text, .Lyrics__Container-sc-1ynbvzw-5, .song__only, #lyrics, #song-body"
     );
     const lyricsText = await page.evaluate(() => {
       const spanElement = document.querySelector(
-        "span.artist_lyrics_text, .Lyrics__Container-sc-1ynbvzw-5, .song_only"
+        "span.artist_lyrics_text, .Lyrics__Container-sc-1ynbvzw-5, song__only, #lyric span , #song-body"
       );
-      return spanElement.textContent;
+      return spanElement.innerHTML;
     });
     lyrics.push(lyricsText);
   }
@@ -51,4 +74,4 @@ const scrapeGoogleLyrics = async (songName, singerName) => {
   return lyrics;
 };
 
-scrapeGoogleLyrics("פאר טסי", "אהבה חולה");
+scrapeGoogleLyrics("Dystinct ft. MHD", "Tek Tek");
