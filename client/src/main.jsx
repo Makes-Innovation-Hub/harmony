da88836c948fa0632c7b5b49c97ba761b4aceed9
Artur Labenskyy <labenskyy@gmail.com>
2023-06-05 11:39:22 +0300

    i18n translate the App

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 import './index.css'
 import { ErrorBoundary } from 'react-error-boundary'
 import { ErrorFallback } from './components/ErrorFallback'
+
+import './i18n'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <ErrorBoundary FallbackComponent={ErrorFallback}>
