ba1a17da7764703d7efe834ce831342535466d4f
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-06-13 16:51:17 +0300

    101_add_language_selection

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,54 @@
 import {
-    Title,
-    HeaderContainer,
-    AppIcon,
-    // TitleFlag
-} from './HeaderStyle'
-import Flag from '../assets/Flag.png'
-import Dove from '../assets/dove.png'
-import Ellipse3 from '../assets/Ellipse3.png'
-import './Header.css'
+  Title,
+  HeaderContainer,
+  AppIcon,
+  // TitleFlag
+} from "./HeaderStyle";
+import Flag from "../assets/Flag.png";
+import Dove from "../assets/dove.png";
+import Ellipse3 from "../assets/Ellipse3.png";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <HeaderContainer>
-            <AppIcon>
-                <img className="dove" src={Dove} alt="" />
-                <img className="ellipse" src={Ellipse3} alt="" />
-            </AppIcon>
-            {/* <TitleFlag> */}
-            <Title>Harmony</Title>
-            <img className="us" src={Flag} alt="Flag" />
-            {/* </TitleFlag> */}
-        </HeaderContainer>
-    )
-}
+  function onClickLang() {
+    console.log("btn clicked");
+  }
 
-export default Header
+  return (
+    <HeaderContainer>
+      <AppIcon>
+        <img className="dove" src={Dove} alt="" />
+        <img className="ellipse" src={Ellipse3} alt="" />
+      </AppIcon>
+      {/* <TitleFlag> */}
+      <Title>Harmony</Title>
+      <button
+        onClick={onClickLang}
+        className="btn-lang"
+        style={{
+          backgroundImage: `url(${Flag})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+        }}
+      ></button>
+      <div className="dropdown">
+        <div className="inner-dropdown">
+          <a href="#">English (US)</a>
+          <img className="us" src={Flag} alt="Flag" />
+        </div>
+        <div className="inner-dropdown">
+          <a href="#">עברית</a>
+          <img className="us" src={Flag} alt="Flag" />
+        </div>
+        <div className="inner-dropdown">
+          <a href="#">عربيه</a>
+          <img className="us" src={Flag} alt="Flag" />
+        </div>
+      </div>
+      {/* <img className="us" src={Flag} alt="Flag" /> */}
+      {/* </TitleFlag> */}
+    </HeaderContainer>
+  );
+};
+
+export default Header;
--- client/src/components/HeaderStyle.jsx
+++ client/src/components/HeaderStyle.jsx
@@ -1,64 +1,64 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0.8rem;
-    width: 100%;
-    @media screen and (min-width: 768px) {
-        padding: 4rem;
-    }
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.8rem;
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    padding: 4rem;
+  }
+`;
 
 export const Title = styled.h1`
-    font-family: 'ABeeZee', sans-serif;
-    font-style: italic;
-    font-weight: 400;
-    width: 35%;
-    font-size: 28px;
-    margin-right: 6rem;
-    margin-left: 1rem;
-    padding-left: 0;
+  font-family: "ABeeZee", sans-serif;
+  font-style: italic;
+  font-weight: 400;
+  width: 35%;
+  font-size: 28px;
+  margin-right: 6rem;
+  margin-left: 1rem;
+  padding-left: 0;
 
-    @media screen and (max-width: 767px) {
-        width: auto;
-        margin-right: 1rem;
-        flex-grow: 1;
-    }
-    @media screen and (max-width: 280px) {
-        width: auto;
-        margin-right: 1rem;
-    }
-`
+  @media screen and (max-width: 767px) {
+    width: auto;
+    margin-right: 1rem;
+    flex-grow: 1;
+  }
+  @media screen and (max-width: 280px) {
+    width: auto;
+    margin-right: 1rem;
+  }
+`;
 
 export const AppIcon = styled.div`
-    position: relative;
-    font-size: 1.5rem;
-    margin-right: 2rem;
-    width: 2%;
-    @media screen and (max-width: 280px) {
-        width: auto;
-        margin-right: 2.7rem;
-    }
-    @media screen and (max-width: 1280px) {
-        width: auto;
-        margin-right: 2.7rem;
-    }
-`
+  position: relative;
+  font-size: 1.5rem;
+  margin-right: 2rem;
+  width: 2%;
+  @media screen and (max-width: 280px) {
+    width: auto;
+    margin-right: 2.7rem;
+  }
+  @media screen and (max-width: 1280px) {
+    width: auto;
+    margin-right: 2.7rem;
+  }
+`;
 
 export const LanguageSelect = styled.select`
-    font-size: 1rem;
-    border: none;
-    appearance: none;
-    background-color: transparent;
-    cursor: pointer;
+  font-size: 1rem;
+  border: none;
+  appearance: none;
+  background-color: transparent;
+  cursor: pointer;
 
-    @media screen and (min-width: 768px) {
-        font-size: 1.2rem;
-    }
-`
+  @media screen and (min-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
 
 export const LanguageOption = styled.option`
-    color: #333;
-`
+  color: #333;
+`;
