81305b572f1e56f399b9ee323f2e0cc5bc252599
daniahih <daniahih310@gmail.com>
2023-06-13 20:33:09 +0300

    fixed small issue

--- client/src/components/TopSongGallary/ImageBoxWithDetails.jsx
+++ client/src/components/TopSongGallary/ImageBoxWithDetails.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Image, ImageBox, SongP, Songartist } from "./TopSongGallaryStyle";
 import { useNavigate } from "react-router-dom";
 import { useGetArtistDataQuery } from "../../api/artistApiSlice"; // Import the artistApiSlice
@@ -14,26 +14,28 @@ export default function ImageBoxWithDetails({
   const [isQueryExecuted, setIsQueryExecuted] = useState(false);
 
   // Use the useGetArtistDataQuery hook to fetch artist data from the slice
-  const { data } = useGetArtistDataQuery(artist, {
+  const { data, isLoading } = useGetArtistDataQuery(artist, {
     skip: !isQueryExecuted, // Skip the query if isQueryExecuted is false
   });
 
   const handleArtistClick = () => {
-    if (data) {
+    setIsQueryExecuted(true); // Set the flag to true when the artist is clicked
+  };
+
+  // Trigger navigation when the data is available
+  useEffect(() => {
+    if (!isLoading && data) {
       const artistInfo = data;
       setArtistData(artistInfo);
       navigate("/Artist", { state: { artistData: artistInfo } });
-    } else {
-      setIsQueryExecuted(true); // Set the flag to true when the artist is clicked
     }
-  };
+  }, [data, isLoading, navigate]);
 
   return (
     <ImageBox onClick={onClick}>
       <Image src={img}></Image>
       <Box>
         <SongP>{songName}</SongP>
-        <Songartist>{artist}</Songartist>
         <Songartist onClick={handleArtistClick}>{artist}</Songartist>
       </Box>
     </ImageBox>
