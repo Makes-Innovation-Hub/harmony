99d8cfb7c7dafe6ae64c4d7dbe1c556d28f87fde
daniahih <daniahih310@gmail.com>
2023-06-14 17:12:49 +0300

    language-dropdown

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,84 @@
+import { useState } from "react";
 import {
-    Title,
-    HeaderContainer,
-    AppIcon,
-    // TitleFlag
-} from './HeaderStyle'
-import Flag from '../assets/Flag.png'
-import Dove from '../assets/dove.png'
-import Ellipse3 from '../assets/Ellipse3.png'
-import './Header.css'
+  Title,
+  HeaderContainer,
+  AppIcon,
+  Us,
+  Option,
+  CountryFlag,
+  LanguageList,
+  LanguageP,
+  LanguageHAP,
+  // TitleFlag
+} from "./HeaderStyle";
+import USAFlag from "../assets/USA.png";
+import palestineFlag from "../assets/palestine.jpg";
+import isrealFlag from "../assets/isreal.jpg";
+import Dove from "../assets/dove.png";
+import Ellipse3 from "../assets/Ellipse3.png";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <HeaderContainer>
-            <AppIcon>
-                <img className="dove" src={Dove} alt="" />
-                <img className="ellipse" src={Ellipse3} alt="" />
-            </AppIcon>
-            {/* <TitleFlag> */}
-            <Title>Harmony</Title>
-            <img className="us" src={Flag} alt="Flag" />
-            {/* </TitleFlag> */}
-        </HeaderContainer>
-    )
-}
+  const [showFlag, setShowFlag] = useState(true);
+  const [showList, setShowList] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [selectedFlag, setSelectedFlag] = useState(USAFlag);
 
-export default Header
+  const handleClick = () => {
+    setShowFlag(false);
+    setShowList(true);
+  };
+
+  const handleLanguageSelect = (language) => {
+    setSelectedLanguage(language);
+
+    if (language === "English") {
+      setSelectedFlag(USAFlag);
+    } else if (language === "Hebrew") {
+      setSelectedFlag(isrealFlag);
+    } else if (language === "Arabic") {
+      setSelectedFlag(palestineFlag);
+    }
+
+    setShowList(false);
+    setShowFlag(true);
+  };
+
+  return (
+    <HeaderContainer>
+      <AppIcon>
+        <img className="dove" src={Dove} alt="" />
+        <img className="ellipse" src={Ellipse3} alt="" />
+      </AppIcon>
+
+      <Title>Harmony</Title>
+
+      {showFlag && (
+        <div onClick={handleClick}>
+          <Us src={selectedFlag} alt="Flag" />
+        </div>
+      )}
+      {showList && (
+        <div className="dropdown">
+          <LanguageList>
+            <Option onClick={() => handleLanguageSelect("English")}>
+              <LanguageP>English (US)</LanguageP>
+              <CountryFlag src={USAFlag} alt="English Flag" />
+            </Option>
+            <Option onClick={() => handleLanguageSelect("Hebrew")}>
+              <LanguageHAP>עִברִית</LanguageHAP>
+              <CountryFlag src={isrealFlag} alt="Hebrew Flag" />
+            </Option>
+            <Option onClick={() => handleLanguageSelect("Arabic")}>
+              <LanguageHAP>العربية</LanguageHAP>
+              <CountryFlag src={palestineFlag} alt="Arabic Flag" />
+            </Option>
+          </LanguageList>
+        </div>
+      )}
+      {/* <TitleFlag> */}
+    </HeaderContainer>
+  );
+};
+
+export default Header;
--- client/src/components/HeaderStyle.jsx
+++ client/src/components/HeaderStyle.jsx
@@ -1,64 +1,108 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0.8rem;
-    width: 100%;
-    @media screen and (min-width: 768px) {
-        padding: 4rem;
-    }
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.8rem;
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    padding: 4rem;
+  }
+`;
 
 export const Title = styled.h1`
-    font-family: 'ABeeZee', sans-serif;
-    font-style: italic;
-    font-weight: 400;
-    width: 35%;
-    font-size: 28px;
-    margin-right: 6rem;
-    margin-left: 1rem;
-    padding-left: 0;
+  font-family: "ABeeZee", sans-serif;
+  font-style: italic;
+  font-weight: 400;
+  width: 35%;
+  font-size: 28px;
+  margin-right: 6rem;
+  margin-left: 1rem;
+  padding-left: 0;
 
-    @media screen and (max-width: 767px) {
-        width: auto;
-        margin-right: 1rem;
-        flex-grow: 1;
-    }
-    @media screen and (max-width: 280px) {
-        width: auto;
-        margin-right: 1rem;
-    }
-`
+  @media screen and (max-width: 767px) {
+    width: auto;
+    margin-right: 1rem;
+    flex-grow: 1;
+  }
+  @media screen and (max-width: 280px) {
+    width: auto;
+    margin-right: 1rem;
+  }
+`;
 
 export const AppIcon = styled.div`
-    position: relative;
-    font-size: 1.5rem;
-    margin-right: 2rem;
-    width: 2%;
-    @media screen and (max-width: 280px) {
-        width: auto;
-        margin-right: 2.7rem;
-    }
-    @media screen and (max-width: 1280px) {
-        width: auto;
-        margin-right: 2.7rem;
-    }
-`
+  position: relative;
+  font-size: 1.5rem;
+  margin-right: 2rem;
+  width: 2%;
+  @media screen and (max-width: 280px) {
+    width: auto;
+    margin-right: 2.7rem;
+  }
+  @media screen and (max-width: 1280px) {
+    width: auto;
+    margin-right: 2.7rem;
+  }
+`;
 
 export const LanguageSelect = styled.select`
-    font-size: 1rem;
-    border: none;
-    appearance: none;
-    background-color: transparent;
-    cursor: pointer;
+  font-size: 1rem;
+  border: none;
+  appearance: none;
+  background-color: transparent;
+  cursor: pointer;
 
-    @media screen and (min-width: 768px) {
-        font-size: 1.2rem;
-    }
-`
+  @media screen and (min-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
 
 export const LanguageOption = styled.option`
-    color: #333;
-`
+  color: #333;
+`;
+
+export const Dropdown = styled.div`
+  position: absolute;
+  top: 4%;
+  left: 80%;
+  /* Other styles */
+`;
+export const Us = styled.img`
+  width: 1.3rem;
+  height: 1.3rem;
+  cursor: pointer;
+  border-radius: 70%;
+  display: ${({ dropdownvisible }) => (dropdownvisible ? "none" : "block")};
+`;
+export const CountryFlag = styled.img`
+  width: 1.2rem;
+  height: 1.2rem;
+  cursor: pointer;
+  border-radius: 50%;
+  margin-right: 3%;
+`;
+export const Option = styled.li`
+  width: 100%;
+  /* border: 1px solid black; */
+  display: flex;
+  justify-content: space-between;
+  padding: 2%;
+`;
+export const LanguageList = styled.ul`
+  /* border: 2px solid red; */
+  width: 120px;
+  margin-top: 1rem;
+`;
+export const LanguageP = styled.p`
+  font-size: 0.7rem;
+  font-family: "ABeeZee";
+  font-weight: bold;
+`;
+export const LanguageHAP = styled.p`
+  font-size: 0.7rem;
+  font-family: "ABeeZee";
+  margin-left: 35%;
+  font-weight: bold;
+`;
