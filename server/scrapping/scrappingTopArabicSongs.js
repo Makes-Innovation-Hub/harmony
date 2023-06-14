cd9e782c557107266a41327e746671e11227f09b
Eyal-donut <eyallila2@gmail.com>
2023-06-14 13:48:55 +0300

    commented_out_coverArt_in_scrape_arabic_top_songs

--- server/scrapping/scrappingTopArabicSongs.js
+++ server/scrapping/scrappingTopArabicSongs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import logger from "../logger.js";
 import * as cheerio from "cheerio";
 import { arabTopSongsUrl } from "../constants/urls.js";
-import { getCoverArtForSong } from "../spotifyapi.js";
+// import { getCoverArtForSong } from "../spotifyapi.js";
 
 async function scrapeTopArabicSongs(req, res) {
   logger.info("starting to scrap top arabic songs");
@@ -22,11 +22,11 @@ async function scrapeTopArabicSongs(req, res) {
         const formattedArtist = artist.replace(/^\d+\.\s*/, "");
         songData.artist = formattedArtist;
         songData.song = song;
-        const coverResult = await getCoverArtForSong(song, artist);
-        logger.info(
-          `cover art found for song name: ${song} and artist name: ${artist}`
-        );
-        songData.coverArt = coverResult;
+        // const coverResult = await getCoverArtForSong(song, artist);
+        // logger.info(
+        //   `cover art found for song name: ${song} and artist name: ${artist}`
+        // );
+        // songData.coverArt = coverResult;
         results.push(songData);
         count++;
         if (count === 10) {
