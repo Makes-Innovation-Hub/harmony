3ba9c3730ba7d7d79b4802038e15e05ad8fdfaa6
Eyal-donut <eyallila2@gmail.com>
2023-06-19 11:46:39 +0300

    .

--- client/src/api/arabicApiSlice.js
+++ client/src/api/arabicApiSlice.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { serverApiUrl } from "../constants/urls";
-//Change this to POST instead of
 export const arabicSongsApi = createApi({
   reducerPath: "arabicSongsApi",
   baseQuery: fetchBaseQuery({ baseUrl: serverApiUrl }),
