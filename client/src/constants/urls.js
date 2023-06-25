e67521ed1f7f22cec31ef2b73b82b84f03eab4f9
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-06-25 20:41:49 +0300

    #add_baseUrl

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,3 +1,4 @@
 const port = import.meta.env.VITE_SERVER_PORT;
+const baseUrl = import.meta.env.VITE_SERVER_BASE_URL;
 
-export const serverApiUrl = `http://localhost:${port}/api/v1`;
+export const serverApiUrl = `${baseUrl}:${port}/api/v1`;
--- scripts/generateConfigClient.sh
+++ scripts/generateConfigClient.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 read -p "PORT: " port
+read -p "BASE URL: " baseUrl
 touch ./client/.env
 echo '
   PORT='$port'
+  VITE_SERVER_BASE_URL='$baseUrl'
 ' > ./client/.env
