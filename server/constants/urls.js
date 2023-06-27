42c734a7bfd107228baaddfc6b8d9ed81dd5d50b
yishain11 <yishain11@walla.co.il>
2023-06-27 10:22:43 +0300

    fixed localhost ref in server

--- server/constants/urls.js
+++ server/constants/urls.js
@@ -1,6 +1,7 @@
 const port = process.env.PORT;
+const baseServerUrl = process.env.BASE_SERVER_URL;
 
-export const serverApiUrl = `http://localhost:${port}/api/v1`;
+export const serverApiUrl = `${baseServerUrl}:${port}/api/v1`;
 
 export const hebrewTopSongsUrl =
   "https://glz.co.il/%D7%92%D7%9C%D7%92%D7%9C%D7%A6/%D7%9E%D7%A6%D7%A2%D7%93%D7%99%D7%9D/%D7%94%D7%9E%D7%A6%D7%A2%D7%93-%D7%94%D7%99%D7%A9%D7%A8%D7%90%D7%9C%D7%99";
