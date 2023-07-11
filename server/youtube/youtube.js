9ec7b5c32eedf31f216b874a87a2a914f70b0390
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-07-11 20:21:22 +0300

    youtube route

---
+++ server/youtube/youtube.js
@@ -0,0 +1,40 @@
+import { GoogleSearch } from "google-search-results-nodejs";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, "../config/config.env") });
+
+const serapApiKey = process.env.SERAP_API_KEY;
+
+const getYoutubeIframe = async (req, res) => {
+  const search = new GoogleSearch(serapApiKey);
+
+  const songDetails = req.body.songDetails;
+  console.log("RE BOSY IS", songDetails);
+
+  const params = {
+    engine: "youtube",
+    search_query: songDetails,
+  };
+
+  function getYouTubeId(url) {
+    var regExp =
+      /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return match && match[7].length == 11 ? match[7] : false;
+  }
+
+  const callback = function (data) {
+    const youtubeUrl = data["video_results"][0].link;
+    const youtubeUrlID = getYouTubeId(youtubeUrl);
+    res.json({ songId: youtubeUrlID });
+  };
+
+  // Show result as JSON
+  search.json(params, callback);
+};
+export default getYoutubeIframe;
