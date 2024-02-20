121c43fb75bed9a39a25d61722f71cb8425429bf
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2024-02-20 17:09:50 +0200

    getPlaylistData and getProfilePic functions are complete. Cleaned the code and added documentation.

--- server/controllers/playlistController.js
+++ server/controllers/playlistController.js
@@ -1,65 +1,40 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-/*
-  TO-DO
-
-  If totalResults>5 fetch again using the next page token to get a total of upto 10 results total
-
-  Create another function that returns the channel profile pic url (artistPic) for a certain song -V
-
-  instead of getting the id from the body change it to query params -V
-
-*/
 // GET Playlist data
 // localhost:5000/api/v1/playlist/?id=PLAYLIST_ID
 export const getPlaylistData = async (req, res) => {
-  // const { id } = req.body;
+  // Extract id from query params and verify that it was provided
   const { id } = req.query;
   if (!id) {
     res.status(400).send("ERROR: id is required");
   }
 
+  // Get the YOUTUBE_API_KEY from process.env and check if it's not null
   const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
   if (!YOUTUBE_API_KEY) {
     res.status(400).send("ERROR: YOUTUBE_API_KEY is required");
   }
 
-  /*
-  // playlistData is an object that contains playlist ID, an array of items objects which contains the names of the videos in the playlist, their thumbnailURL which is the high res thumbnail url from the response, and video's channel name, videos channel profile pic url, and channelID,  
-  // Since there is paging, when we fetch the playlistItems we get an array of 5 objects and if there's more than 5 there's a nextPageToken, so if the song has more than 5 also get the videos from the next page (so in total i want the max number of videos to be returned is 10 videos/items if there's only 5 vids or less no need to do next page token check)
-  const playlistData = {}
-
-    // CONTINUE WITH THE CURRENT CODE
-
-    // Replace this with 
-  res.status(200).send(`ID: ${id}\nKEY:${YOUTUBE_API_KEY}`);
-
-  */
-
   try {
-    // Fetch playlist items (videos)
+    // Fetch playlist items (videos) with maxResults = 10
     const playlistItemsResponse = await fetch(
-      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&key=${YOUTUBE_API_KEY}`
+      `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&maxResults=10&key=${YOUTUBE_API_KEY}`
     );
-    console.log("playlistItemsResponse", playlistItemsResponse);
+
+    // Check if fetching failed
     if (!playlistItemsResponse.ok) {
       throw new Error("Failed to fetch playlist information");
     }
+    // Convert the fetched data into a json object
     const playlistItemsData = await playlistItemsResponse.json();
 
+    // Make sure the playlist is not empty
     if (!playlistItemsData.items || playlistItemsData.items.length === 0) {
       throw new Error("Playlist not found or empty");
     }
 
-    // Fetch playlist items (videos)
-    // const playlistItemsResponse = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${id}&key=${YOUTUBE_API_KEY}`);
-    // if (!playlistItemsResponse.ok) {
-    //   throw new Error('Failed to fetch playlist items');
-    // }
-    // const playlistItemsData = await playlistItemsResponse.json();
-
-    // Extract relevant information from the responses
+    // Extract relevant information from the response
     const playlistInfo = {
       id: id, //playlist id
       items: playlistItemsData.items.map((item) => {
@@ -71,7 +46,6 @@ export const getPlaylistData = async (req, res) => {
           channelId: item.snippet.channelId, //channelId
         };
       }),
-      // allItems: playlistItemsData.items,
     };
 
     // Send the playlist data in the response
@@ -83,34 +57,40 @@ export const getPlaylistData = async (req, res) => {
 };
 
 // GET profilePicUrl of the channel to be in place of the artist picture
+// GET localhost:5000/api/v1/playlist/profilePic/?id=CHANNEL_ID
 export const getChannelProfilePic = async (req, res) => {
+  // extract id from request query params and make sure it's provided
   const { id } = req.query;
-
   if (!id) {
     return res.status(400).send("ERROR: Channel ID is required");
   }
 
+  // set the YOUTUBE_API_KEY to the value in the .env file and check that it's not null
   const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
   if (!YOUTUBE_API_KEY) {
     return res.status(400).send("ERROR: YOUTUBE_API_KEY is required");
   }
 
   try {
+    // Fetch the channel information
     const channelResponse = await fetch(
       `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${id}&key=${YOUTUBE_API_KEY}`
     );
+    // Check if fetching failed
     if (!channelResponse.ok) {
       throw new Error("Failed to fetch channel information");
     }
     const channelData = await channelResponse.json();
 
+    // Check if channel exists
     if (!channelData.items || channelData.items.length === 0) {
       throw new Error("Channel not found");
     }
 
+    // get the profile pic url from the response (default size)
     const profilePicUrl = channelData.items[0].snippet.thumbnails.default.url;
-    // const profilePicUrl = channelData.items[0].snippet.thumbnails.medium.url;
 
+    // Send profilePicUrl
     res.status(200).json({ profilePicUrl });
   } catch (error) {
     console.error("Error fetching channel profile picture:", error);
--- server/routes/playlistRoutes.js
+++ server/routes/playlistRoutes.js
@@ -9,7 +9,7 @@ const router = Router();
 // GET localhost:5000/api/v1/playlist/data/?id=PLAYLIST_ID
 router.get("/playlistData", getPlaylistData);
 
-// GET localhost:5000/api/v1/playlist/profilePic/?id=PLAYLIST_ID
+// GET localhost:5000/api/v1/playlist/profilePic/?id=CHANNEL_ID
 router.get("/profilePic", getChannelProfilePic);
 
 export default router;
