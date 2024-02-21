030757d84d4e433001ad8e296e9b11d380c3eeed
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2024-02-21 12:46:34 +0200

    playlist data returns channel profile pic

--- server/controllers/playlistController.js
+++ server/controllers/playlistController.js
@@ -36,7 +36,7 @@ export const getPlaylistData = async (req, res) => {
     }
 
     // Extract relevant information from the response
-    const playlistInfo = {
+    /*const playlistInfo = {
       id: id, //playlist id
       items: playlistItemsData.items.map((item) => {
         return {
@@ -47,10 +47,27 @@ export const getPlaylistData = async (req, res) => {
           channelId: item.snippet.channelId, //channelId
         };
       }),
-    };
+    };*/
+
+    const itemsWithProfilePic = await Promise.all(
+      playlistItemsData.items.map(async (item) => {
+        const profilePicUrl = await fetchChannelProfilePic(
+          item.snippet.videoOwnerChannelId
+        );
+        return {
+          videoId: item.snippet.resourceId.videoId,
+          title: item.snippet.title,
+          thumbnailUrl: item.snippet.thumbnails.standard.url,
+          channelTitle: item.snippet.videoOwnerChannelTitle,
+          channelId: item.snippet.videoOwnerChannelId,
+          profilePicUrl: profilePicUrl,
+        };
+      })
+    );
 
     // Send the playlist data in the response
-    res.status(200).json(playlistInfo);
+    // res.status(200).json(playlistInfo);
+    res.status(200).json(itemsWithProfilePic);
   } catch (error) {
     console.error("Error fetching playlist data:", error);
     res.status(500).send("Internal Server Error");
@@ -105,4 +122,35 @@ export const getChannelProfilePic = async (req, res) => {
 export const getAllPlaylistsData = (req, res) => {
   const playlists = playlistData;
   res.send(playlists);
+};
+
+//NEW
+
+// Function to fetch profile picture URL for a channel ID
+const fetchChannelProfilePic = async (channelId) => {
+  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+  if (!YOUTUBE_API_KEY) {
+    throw new Error("YOUTUBE_API_KEY is required");
+  }
+
+  try {
+    const channelResponse = await fetch(
+      `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${YOUTUBE_API_KEY}`
+    );
+
+    if (!channelResponse.ok) {
+      throw new Error("Failed to fetch channel information");
+    }
+
+    const channelData = await channelResponse.json();
+
+    if (!channelData.items || channelData.items.length === 0) {
+      throw new Error("Channel not found");
+    }
+
+    return channelData.items[0].snippet.thumbnails.default.url;
+  } catch (error) {
+    console.error("Error fetching channel profile picture:", error);
+    throw error;
+  }
 };
