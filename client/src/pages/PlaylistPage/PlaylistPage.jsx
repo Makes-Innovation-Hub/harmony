0f480fca57d940078ded89cfda1ddbbf6a0ce23a
osaydayoub <osaydayoub@gmail.com>
2024-02-24 00:10:18 +0200

    style change

--- client/src/pages/PlaylistPage/PlaylistPage.jsx
+++ client/src/pages/PlaylistPage/PlaylistPage.jsx
@@ -23,49 +23,28 @@ function PlaylistPage() {
     }
   }, [isSuccess]);
   return (
-    // <>
-    //   <Header />
-    //   <h3>{currentPlaylistData.playlistName}</h3>
-    //   {isSuccess && (
-    //     <div>
-    //       {playlistQueryData.map((song, index) => {
-    //         return (
-    //           <div key={index}>
-    //             <h3>{song.songName3Lang.english}</h3>
-    //             <h3>{song.songName3Lang.arabic}</h3>
-    //             <h3>{song.songName3Lang.hebrew}</h3>
-    //             <ImgCard src={song.thumbnailUrl} />
-    //             {/* <ImgCard src={song.profilePicUrl} /> */}
-
-    //           </div>
-    //         );
-    //       })}
-    //     </div>
-    //   )}
-    // </>
     <PageWrapper>
-      <FlexGrowContainer flexGrow="1">
-        <Header />
-      </FlexGrowContainer>
-      <FlexGrowContainer flexGrow="2" padding="0 0.8rem">
-        <h3>{currentPlaylistData.playlistName}</h3>
-      </FlexGrowContainer>
-
+      <Header />
       {isSuccess && (
-        <FlexGrowContainer flexGrow="6" padding="0 0.8rem">
-          <ContentWrapper>
-            {playlistQueryData.map((song, index) => (
-              <SongInPlaylist
-                key={index}
-                artist={"artistData.name.hebrew"}
-                arabicName={song.songName3Lang.arabic}
-                hebrewName={song.songName3Lang.hebrew}
-                englishName={song.songName3Lang.english}
-                imgURL={song.thumbnailUrl}
-              />
-            ))}
-          </ContentWrapper>
-        </FlexGrowContainer>
+        <>
+          <FlexGrowContainer flexGrow="2" padding="0 0.8rem">
+            <h3>{currentPlaylistData.playlistName}</h3>
+          </FlexGrowContainer>
+          <FlexGrowContainer flexGrow="6" padding="0 0.8rem">
+            <ContentWrapper>
+              {playlistQueryData.map((song, index) => (
+                <SongInPlaylist
+                  key={index}
+                  artist={"artistData.name.hebrew"}
+                  arabicName={song.songName3Lang.arabic}
+                  hebrewName={song.songName3Lang.hebrew}
+                  englishName={song.songName3Lang.english}
+                  imgURL={song.thumbnailUrl}
+                />
+              ))}
+            </ContentWrapper>
+          </FlexGrowContainer>
+        </>
       )}
     </PageWrapper>
   );
