dbcaa2001ff12ba787e4df4702e3bc720df94e7a
osaydayoub <osaydayoub@gmail.com>
2024-02-25 14:01:04 +0200

    style added

--- client/src/components/SongInPlaylist/SongInPlaylist.jsx
+++ client/src/components/SongInPlaylist/SongInPlaylist.jsx
@@ -3,7 +3,13 @@ import ImgCard from "../General/ImgCard";
 import ThreeLangNames from "../ThreeLnagNames/ThreeLangNames";
 import { ContentWrapper, ContentWrapper2 } from "./SongInPlaylist.styled";
 
-function SongInPlaylist({ arabicName, hebrewName, englishName, imgURL }) {
+function SongInPlaylist({
+  arabicName,
+  hebrewName,
+  englishName,
+  imgURL,
+  isPlaying,
+}) {
   const currentPlaylistData = useSelector((state) => state.currentplaylist);
   // TODO: Check if the song index matches the current song index and define the isPlaying value accordingly
   return (
@@ -18,7 +24,7 @@ function SongInPlaylist({ arabicName, hebrewName, englishName, imgURL }) {
           englishName={englishName}
           fontSize="13px"
           lineHeight="17.29px"
-          isPlaying={false}
+          isPlaying={isPlaying}
         />
       </ContentWrapper2>
     </ContentWrapper>
