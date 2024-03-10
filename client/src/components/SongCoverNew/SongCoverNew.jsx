cec888941d0e3e48785ae497c969de8066a3b274
HasanOmar1 <r3domar1@gmail.com>
2024-03-10 10:36:57 +0200

    created new component with the name SongCoverNew to resolve issues with netlify

---
+++ client/src/components/SongCoverNew/SongCoverNew.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import {
+  CoverBox,
+  TitleContainer,
+  ByArtist,
+  ImgStyles,
+  LikesAndViewsContainer,
+} from "./SongCoverStyles";
+
+function SongCoverNew({ backgroundImg, artist, state, likes, views }) {
+  return (
+    <>
+      <TitleContainer>
+        <ByArtist>By {artist}</ByArtist>
+      </TitleContainer>
+      <CoverBox>
+        <Link to={"/cover"} state={state}>
+          <ImgStyles
+            width="560"
+            height="255"
+            src={`https://img.youtube.com/vi/${backgroundImg}/maxresdefault.jpg`}
+            alt="Song cover"
+          ></ImgStyles>
+        </Link>
+      </CoverBox>
+      <LikesAndViewsContainer>
+        <p>{views} views</p>
+        <p>{likes} likes</p>
+      </LikesAndViewsContainer>
+    </>
+  );
+}
+
+export default SongCoverNew;
