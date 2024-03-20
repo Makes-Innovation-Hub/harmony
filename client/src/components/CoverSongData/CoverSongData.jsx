36df529166e7a47f9b8d0880ed515a41927003f5
HasanOmar1 <r3domar1@gmail.com>
2024-03-20 13:38:14 +0200

    merged playlist feature with songPage-Covers

--- client/src/components/CoverSongData/CoverSongData.jsx
+++ client/src/components/CoverSongData/CoverSongData.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { CoversTitle, SongCoverVideoContainer } from "./CoverSongDataStyles";
 import SongCoverNew from "../SongCoverNew/SongCoverNew";
 
-export default function CoverSongData({ songData, songByIdData }) {
+export default function CoverSongData({ songByIdData }) {
   return (
     <div>
-      {songData?.coverSong.length > 0 && <CoversTitle>Covers</CoversTitle>}
+      {songByIdData?.coverSong.length > 0 && <CoversTitle>Covers</CoversTitle>}
       <SongCoverVideoContainer>
         {songByIdData?.coverSong.map((coverInfo) => {
           return (
--- client/src/pages/SongPage.jsx
+++ client/src/pages/SongPage.jsx
@@ -13,12 +13,12 @@ import Animation from "../components/Animation/Animation.component";
 import translatingGif from "../assets/animations/translating-animation.gif";
 
 function SongPage() {
-  const songData = useLocation().state;
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const { data: songByIdData } = useGetSongByIdQuery(songData?._id, {
-    skip: !songData?._id,
-  });
+  const { id } = useParams();
+  const { data: songByIdData, isSuccess: fetchedSongData } =
+    useGetSongByIdQuery(id, {
+      skip: !id,
+    });
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -27,80 +27,51 @@ function SongPage() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  const { id } = useParams();
-  const { data: songByIdData, isSuccess: fetchedSongData } =
-    useGetSongByIdQuery(id, {
-      skip: !id,
-    });
 
   return (
     <>
       <Header />
-      <S.SongPageContainer>
-        <S.SongInfoAndVideoContainer>
-          <S.SongInfoContainer>
-            <SongDetails
-              img={songData.coverArt}
-              artist={songData.artist.name.english}
-              songName={songData.name.english}
-              originalLang={songData.originalLang}
-            />
-            <Lyrics
-              originalLang={songData.originalLang}
-              lyrics={songData.lyrics}
-              name={songData.name}
-            />
-          </S.SongInfoContainer>
-          <Youtube
-            songName={songData.name.hebrew}
-            artistName={songData.artist.name.hebrew}
-            youtubeUrl={songData.youtubeURL}
-          />
-        </S.SongInfoAndVideoContainer>
-
-        <S.UploadCoverButton onClick={openModal}>
-          Upload Cover
-        </S.UploadCoverButton>
-
-        <GenericModal isOpen={isModalOpen} onRequestClose={closeModal}>
-          <UploadCoverForm
-            originalArtist={songData.artist.name.english}
-            originalLang={songData.originalLang}
-            originalSongCover={songData.coverArt}
-            originalSongName={songData.name.english}
-            originalSongId={songData._id}
-            closeModal={closeModal}
-          />
-        </GenericModal>
-
-        <CoverSongData songData={songData} songByIdData={songByIdData} />
-      </S.SongPageContainer>
       {fetchedSongData ? (
         <>
-          <SongDetails
-            img={songByIdData.coverArt}
-            artist={songByIdData.artist.name.english}
-            songName={songByIdData.name.english}
-            originalLang={songByIdData.originalLang}
-          />
-          <Lyrics
-            originalLang={songByIdData.originalLang}
-            lyrics={songByIdData.lyrics}
-            name={songByIdData.name}
-          />
-          <Youtube
-            songName={songByIdData.name.hebrew}
-            artistName={songByIdData.artist.name.hebrew}
-            youtubeUrl={songByIdData.youtubeURL}
-          />
-          <UploadCoverModal
-            originalArtist={songByIdData.artist.name.english}
-            originalLang={songByIdData.originalLang}
-            originalSongCover={songByIdData.coverArt}
-            originalSongName={songByIdData.name.english}
-            originalSongId={songByIdData._id}
-          />
-          <CoverSongData songData={songByIdData} songByIdData={songByIdData} />
+          <S.SongPageContainer>
+            <S.SongInfoAndVideoContainer>
+              <S.SongInfoContainer>
+                <SongDetails
+                  img={songByIdData.coverArt}
+                  artist={songByIdData.artist.name.english}
+                  songName={songByIdData.name.english}
+                  originalLang={songByIdData.originalLang}
+                />
+                <Lyrics
+                  originalLang={songByIdData.originalLang}
+                  lyrics={songByIdData.lyrics}
+                  name={songByIdData.name}
+                />
+              </S.SongInfoContainer>
+              <Youtube
+                songName={songByIdData.name.hebrew}
+                artistName={songByIdData.artist.name.hebrew}
+                youtubeUrl={songByIdData.youtubeURL}
+              />
+            </S.SongInfoAndVideoContainer>
+
+            <S.UploadCoverButton onClick={openModal}>
+              Upload Cover
+            </S.UploadCoverButton>
+
+            <GenericModal isOpen={isModalOpen} onRequestClose={closeModal}>
+              <UploadCoverForm
+                originalArtist={songByIdData.artist.name.english}
+                originalLang={songByIdData.originalLang}
+                originalSongCover={songByIdData.coverArt}
+                originalSongName={songByIdData.name.english}
+                originalSongId={songByIdData._id}
+                closeModal={closeModal}
+              />
+            </GenericModal>
+
+            <CoverSongData songByIdData={songByIdData} />
+          </S.SongPageContainer>
         </>
       ) : (
         <Animation
