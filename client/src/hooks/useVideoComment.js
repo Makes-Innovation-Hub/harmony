f019030abb9e21422c8e4bc07ee5260c2bbe2b33
HasanOmar1 <r3domar1@gmail.com>
2024-03-27 13:36:46 +0200

    added custom hooks for CoverPage

---
+++ client/src/hooks/useVideoComment.js
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+import { useAddCommentMutation } from "../api/viewsAndLikesApi";
+
+function useVideoComment(updatedCoverSong) {
+  const [isCommenting, setIsCommenting] = useState(false);
+  const commentRef = useRef();
+  const [addComment] = useAddCommentMutation();
+
+  useEffect(() => {
+    if (isCommenting) {
+      commentRef.current.focus();
+    }
+  }, [isCommenting]);
+
+  const handleShowComment = () => {
+    setIsCommenting((prev) => !prev);
+  };
+
+  function handleAddComment() {
+    addComment({
+      id: updatedCoverSong?._id,
+      content: commentRef?.current.value,
+    });
+    commentRef.current.value = "";
+  }
+
+  return [isCommenting, commentRef, handleShowComment, handleAddComment];
+}
+
+export default useVideoComment;
