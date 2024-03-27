f4fdf45e4e3aabf8cf77ccaa2547d93d0cf6647a
HasanOmar1 <r3domar1@gmail.com>
2024-03-27 10:50:02 +0200

    removed comments

--- client/src/components/TopSongGallary/TopSongGallary.jsx
+++ client/src/components/TopSongGallary/TopSongGallary.jsx
@@ -58,35 +58,5 @@ export default function TopSongGallary() {
         </>
       )}
     </>
-    // <SongGallary>
-    //   <TopHSongCountainer>
-    //     <Title>{t("top_hebrew")}</Title>
-    //     <ImageBoxContainer>
-    //       {topSongsAll.data &&
-    //         topSongsAll.data.hebrewSongs.map((song, index) => (
-    //           <ImageBoxWithDetails
-    //             key={index}
-    //             img={song.coverArt}
-    //             artist={song.artist}
-    //             songName={song.song}
-    //           />
-    //         ))}
-    //     </ImageBoxContainer>
-    //   </TopHSongCountainer>
-    //   <TopASongCountainer>
-    //     <Title>{t("top_arabic")}</Title>
-    //     <ImageBoxContainer>
-    //       {topSongsAll.data &&
-    //         topSongsAll.data.arabicSongs.map((song, index) => (
-    //           <ImageBoxWithDetails
-    //             key={index}
-    //             img={song.coverArt}
-    //             artist={song.artist}
-    //             songName={song.song}
-    //           />
-    //         ))}
-    //     </ImageBoxContainer>
-    //   </TopASongCountainer>
-    // </SongGallary>
   );
 }
