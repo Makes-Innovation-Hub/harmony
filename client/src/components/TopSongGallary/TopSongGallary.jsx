397322e59255db956494c40578d4c006b0645e1c
HasanOmar1 <r3domar1@gmail.com>
2024-03-31 10:35:20 +0300

    fixed animation in homepage

--- client/src/components/TopSongGallary/TopSongGallary.jsx
+++ client/src/components/TopSongGallary/TopSongGallary.jsx
@@ -8,55 +8,43 @@ import {
 import ImageBoxWithDetails from "./ImageBoxWithDetails";
 import { useGetTopSongsQuery } from "../../api/topSongsSlice";
 import { useTranslation } from "react-i18next";
-import Animation from "../Animation/Animation.component";
-import translatingGif from "../../assets/animations/translating-animation.gif";
 
 export default function TopSongGallary() {
-  const { data: topSongsAll = [], isLoading: topSongsIsLoading } =
-    useGetTopSongsQuery();
+  const { data: topSongsAll = [] } = useGetTopSongsQuery();
   const { t } = useTranslation();
 
   return (
     <>
-      {topSongsIsLoading ? (
-        <Animation
-          animationGif={translatingGif}
-          animationText={["Loading top songs..."]}
-        />
-      ) : (
-        <>
-          <SongGallary>
-            <TopHSongCountainer>
-              <Title>{t("top_hebrew")}</Title>
-              <ImageBoxContainer>
-                {topSongsAll.data &&
-                  topSongsAll.data.hebrewSongs.map((song, index) => (
-                    <ImageBoxWithDetails
-                      key={index}
-                      img={song.coverArt}
-                      artist={song.artist}
-                      songName={song.song}
-                    />
-                  ))}
-              </ImageBoxContainer>
-            </TopHSongCountainer>
-            <TopASongCountainer>
-              <Title>{t("top_arabic")}</Title>
-              <ImageBoxContainer>
-                {topSongsAll.data &&
-                  topSongsAll.data.arabicSongs.map((song, index) => (
-                    <ImageBoxWithDetails
-                      key={index}
-                      img={song.coverArt}
-                      artist={song.artist}
-                      songName={song.song}
-                    />
-                  ))}
-              </ImageBoxContainer>
-            </TopASongCountainer>
-          </SongGallary>
-        </>
-      )}
+      <SongGallary>
+        <TopHSongCountainer>
+          <Title>{t("top_hebrew")}</Title>
+          <ImageBoxContainer>
+            {topSongsAll.data &&
+              topSongsAll.data.hebrewSongs.map((song, index) => (
+                <ImageBoxWithDetails
+                  key={index}
+                  img={song.coverArt}
+                  artist={song.artist}
+                  songName={song.song}
+                />
+              ))}
+          </ImageBoxContainer>
+        </TopHSongCountainer>
+        <TopASongCountainer>
+          <Title>{t("top_arabic")}</Title>
+          <ImageBoxContainer>
+            {topSongsAll.data &&
+              topSongsAll.data.arabicSongs.map((song, index) => (
+                <ImageBoxWithDetails
+                  key={index}
+                  img={song.coverArt}
+                  artist={song.artist}
+                  songName={song.song}
+                />
+              ))}
+          </ImageBoxContainer>
+        </TopASongCountainer>
+      </SongGallary>
     </>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,19 +6,32 @@ import TopSongGallary from "../components/TopSongGallary/TopSongGallary";
 import TopPlaylist from "../components/topPlaylist/topPlaylist";
 import * as S from "./NotFoundPageStyles.js";
 import HarmonyCovers from "../components/HarmonyCovers/HarmonyCovers";
+import { useGetTopSongsQuery } from "../api/topSongsSlice.js";
+import translatingGif from "../assets/animations/translating-animation.gif";
+import Animation from "../components/Animation/Animation.component.jsx";
 
 export default function Home() {
+  const { isLoading: topSongsIsLoading } = useGetTopSongsQuery();
   return (
     <>
       <Header />
-      <S.SeatchContainer>
-        <Tagline />
-        <HomeSearchBar />
-      </S.SeatchContainer>
+      {topSongsIsLoading ? (
+        <Animation
+          animationGif={translatingGif}
+          animationText={["Loading Data..."]}
+        />
+      ) : (
+        <>
+          <S.SeatchContainer>
+            <Tagline />
+            <HomeSearchBar />
+          </S.SeatchContainer>
 
-      <TopSongGallary />
-      <HarmonyCovers />
-      <TopPlaylist />
+          <TopSongGallary />
+          <HarmonyCovers />
+          <TopPlaylist />
+        </>
+      )}
     </>
   );
 }
